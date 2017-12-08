c44101815b031b9c1717b8acc37b46fa1f0da8af
Greg Look <greg@greg-look.net>
2017-12-07 21:21:32 -0800

    Initial project skeleton.

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject mvxcvi/advent-of-code "0.1.0-SNAPSHOT"
+  :description "Advent of Code problems."
+  :url "https://github.com/greglook/advent-of-code"
+
+  :dependencies
+  [[org.clojure/clojure "1.8.0"]])
